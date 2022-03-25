47aa9cca1666d0fb5dbc7f5814a9ca443df84774
Glitch (glitch-hello-website) <none>
2022-03-25 06:24:47 +0000

    👔🌵 Checkpoint ./recipes/script.js:34214088/4461

--- recipes/script.js
+++ recipes/script.js
@@ -57,138 +57,12 @@ function launchTimer() {
 }
 timerField.addEventListener('keydown', function(e) {
     if (e.keyCode === 13) {
-        if (isNaN(timerField.value)) {
-        Swal.fire({
-            title: `Допустим ввод только чисел
-            и знака '.' (точка)`,
-            customClass: 'adaptation',
-          })
-          timerField.value = '';
-    }
-    else if (timerField.value <= 0) {
-        Swal.fire({
-            title: `Число должно быть выше 0`,
-            customClass: 'adaptation',
-          })
-          timerField.value = '';
-    }
-    else {
-        count();
-        timerField.value = '';
-    }
+        launchTimer();
     }
 })
 
-function count() {
-  startTimerBtn.style.display = 'none'; 
-    resumeTimerBtn.style.display = 'none';
-    timerField.style.display = 'none';
-    timerHeader.style.display = 'none';
-    pauseTimerBtn.style.display = 'block';
-    resetTimerBtn.style.display = 'block';
-    counter.style.display = 'block';
-    timeLeftHeader.style.display = 'block';
-
-    let time = timerField.value;
-    let totalTime = Number(time) * 60;
-    let isPaused = false; //setting pause for setInterval, при первичном запуске функции он равен 0 (false)
-
-    let timeID = setInterval(function() {
-        let hours = Math.floor((totalTime/60)/60);
-        if (hours < 10) {
-            hours = '0' + hours;
-        }
-        let minutes = Math.floor(totalTime/60%60);
-        if (minutes < 10) {
-            minutes = '0' + minutes;
-        }
-        let seconds = (totalTime%60).toFixed(0);
-        if (seconds < 10) {
-            seconds = '0' + seconds;
-        }
-        
-        /* Important condition for setting a pause lately */
-        if(!isPaused) { //if the variable hasn't been set yet, do this:
-            counter.textContent = `${hours} : ${minutes} : ${seconds}`;
-            totalTime--;
-        }
-
-        if (totalTime < 0) {
-            pauseTimerBtn.style.display = 'none';
-            clearInterval(timeID);
-            totalTime = 0;
-            counter.textContent = `00 : 00 : 00`;
-            Swal.fire({
-                title: `Время вышло!
-                Проверьте готовность блюда`,
-                imageUrl: 'https://cdn.glitch.global/2352592e-0222-4a73-ae43-2de112bee7dc/alarm_clock.gif?v=1648168558038',
-                imageWidth: 240,
-                imageHeight: 260,
-                customClass: 'adaptation',
-              });
-              audio.play();
-        }
-    }, 1000);
-
-    /* Pausing the countdown */
-    pauseTimerBtn.addEventListener('click', function(e) {
-        e.preventDefault();
-        isPaused = true; //stopped the countdoun
-        resumeTimerBtn.style.display = 'block';
-        pauseTimerBtn.style.display = 'none';
-    });
-
-    /* Resuming the countdown */
-    resumeTimerBtn.addEventListener('click', function(e) {
-        e.preventDefault();
-        isPaused = false; //resumed the countdown
-        resumeTimerBtn.style.display = 'none';
-        pauseTimerBtn.style.display = 'block';
-    })
-
-    /* Reloading the Timer */
-    resetTimerBtn.addEventListener('click', () => {
-        clearInterval(timeID);
-        totalTime = 0;
-        counter.textContent = `00 : 00 : 00`;
-        timerField.style.display = 'block';
-        counter.style.display = 'none';
-        startTimerBtn.style.display = 'block'; 
-        pauseTimerBtn.style.display = 'none';
-        resetTimerBtn.style.display = 'none';
-        resumeTimerBtn.style.display = 'none';
-        timeLeftHeader.style.display = 'none';
-        timerHeader.style.display = 'block';
-        audio.pause();
-    })
-
-    /* Closing Timer */
-    closeTimerBtn.addEventListener('click', hideTimer);
-    function hideTimer() {
-    timerBtn.style.display = 'block';
-    timerContainer.style.display = 'none';
-    headerParent.style.display = 'block';
-    timeLeftHeader.style.display = 'none';
-    timerHeader.style.display = 'block';
-    
-    clearInterval(timeID);
-    totalTime = 0;
-    counter.textContent = `00 : 00 : 00`;
-    timerField.style.display = 'block';
-    counter.style.display = 'none';
-    startTimerBtn.style.display = 'block'; 
-    pauseTimerBtn.style.display = 'none';
-    resetTimerBtn.style.display = 'none';
-    resumeTimerBtn.style.display = 'none';
-    audio.pause();
-}
-}
-
-
-
-
-
 function startCountdown() {
+  clearInterval(timerID);
     startTimerBtn.style.display = 'none'; 
     resumeTimerBtn.style.display = 'none';
     timerField.style.display = 'none';
@@ -221,8 +95,12 @@ function startCountdown() {
             counter.textContent = `${hours} : ${minutes} : ${seconds}`;
             totalTime--;
         }
-
-        if (totalTime < 0) {
+        
+        if (totalTime > 0){
+            audio.pause();
+        }
+        else if (totalTime <= 0) {
+            audio.play();
             pauseTimerBtn.style.display = 'none';
             clearInterval(timerID);
             totalTime = 0;
@@ -235,8 +113,8 @@ function startCountdown() {
                 imageHeight: 260,
                 customClass: 'adaptation',
               });
-              audio.play();
         }
+       
     }, 1000);
 
     /* Pausing the countdown */
@@ -268,7 +146,7 @@ function startCountdown() {
         resumeTimerBtn.style.display = 'none';
         timeLeftHeader.style.display = 'none';
         timerHeader.style.display = 'block';
-        audio.pause();
+        // audio.pause();
     })
 
     /* Closing Timer */
@@ -289,7 +167,7 @@ function startCountdown() {
     pauseTimerBtn.style.display = 'none';
     resetTimerBtn.style.display = 'none';
     resumeTimerBtn.style.display = 'none';
-    audio.pause();
+    // audio.pause();
 }
 };
 /* Closing Timer-2 */
