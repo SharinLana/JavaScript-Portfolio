63004f90ce6e3fed9946be365fbad6d8903dfeb3
Glitch (glitch-hello-website) <none>
2022-04-11 23:49:30 +0000

    📻🎬 Checkpoint ./weather/script.js:34214088/38

--- weather/script.js
+++ weather/script.js
@@ -260,7 +260,7 @@ async function currentLocation() {
     const resultLocation = await fetch(`${apiLocation.endpoint}api_key=${apiLocation.key}`);
     const resultLocationReceived = await resultLocation.json();
         
-    const resultWeather = await fetch(`${api.endpoint}weather?q=${resultLocationReceived.city}&units=metric&appID=${api.key}`)
+    const resultWeather = await fetch(`${api.endpoint}weather?q=${resultLocationReceived.city},${resultLocationReceived.country}&units=metric&appID=${api.key}`)
     const resultWeatherReceived = await resultWeather.json(); 
     console.log(resultWeatherReceived)
     
